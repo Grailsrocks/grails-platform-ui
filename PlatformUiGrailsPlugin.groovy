242a2aec725bc1a99de7e4ea202cdebb218ca7b1
Marc Palmer <marc@anyware.co.uk>
2012-03-27 20:15:27 +0100

    What was I thinking. Not ready yet.

--- PlatformUiGrailsPlugin.groovy
+++ PlatformUiGrailsPlugin.groovy
@@ -21,7 +21,7 @@ import org.springframework.core.io.FileSystemResource
 
 class PlatformUiGrailsPlugin {
     // the plugin version
-    def version = "1.0.M1"
+    def version = "1.0.M1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
